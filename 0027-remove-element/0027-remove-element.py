461fef8236aad2af33f8a358ec2494e959048cba
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-01-23 18:54:38 +0530

    Time: 0 ms (100%), Space: 17.9 MB (7.19%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        result = 0
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[result] = nums[i]
+                result += 1
+        return result
+
+        
