a5b71330211b2b027da594bd85c695aaad81c30c
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-02-22 21:32:14 +0530

    Time: 0 ms (100%), Space: 19.7 MB (23.13%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+        for num in nums:
+            single ^= num
+        return single
+
+
+
+
+        
