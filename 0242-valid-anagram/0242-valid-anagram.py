363fc0d3238a7adefcd8d4e009c97412494a262c
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-03-28 08:30:58 +0530

    Time: 8 ms (79.44%), Space: 17.8 MB (64.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-
-        if len(s) != len(t):
-            return False
-        s.sort()
-        t.sort()
-
-        for i in range(len(s)):
-            if s[i] not in t[i]:
-                return False
-        return True
+        return Counter(s) == Counter(t)
 
         
