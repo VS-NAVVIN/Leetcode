9beeed8a1023ee5e04e654439e9e98f4c96b8715
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-05-11 08:17:54 +0530

    Time: 850 ms (61.58%), Space: 35.2 MB (15.94%) - LeetHub

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        sum1 = sum(nums1)
+        sum2 = sum(nums2)
+        n1 = nums1.count(0)
+        n2 = nums2.count(0)
+        
+        tot = max(sum1+n1, sum2+n2)
+        if sum1 != tot and not n1 or sum2 != tot and not n2:
+            return -1
+        return tot
+                 
+
+        
