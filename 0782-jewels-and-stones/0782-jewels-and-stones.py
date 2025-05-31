6e00ba760ca32aecd84030c49b719d970950a97a
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-05-31 11:29:46 +0530

    Time: 0 ms (100%), Space: 17.5 MB (96.87%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        total = 0
+
+        for i in stones:
+            if i in jewels:
+                total += 1
+
+        return total
