a72c7ed29e8da196ecf357cba1959cebb284183f
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-06-02 09:42:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (8.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        result = [nums[0]]
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                result.append(nums[i])
-        nums[:] = result
-        return len(result)
+        
+        j = 0
+        for i in range(len(nums)):
+            if nums[j] != nums[i]:
+                j += 1
+                nums[j] = nums[i]
+        return j+1
+
 
 
 
