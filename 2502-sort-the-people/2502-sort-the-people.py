cdd4cc601f49dae796729ab196097080379762e7
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-06-05 10:04:51 +0530

    Time: 3 ms (81.13%), Space: 18.4 MB (30.15%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -2,7 +2,7 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         paired_data = zip(names, heights)
         sorted_pairs = sorted(paired_data,key=lambda x: x[1], reverse=True)
-        sorted_names = [name for name, _ in sorted_pairs]
+        sorted_names = [name for name, height in sorted_pairs]
         
         return sorted_names
         
