135e2008b60b229a90626b62fbb5cb9f75581e57
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-08-25 17:24:59 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        
+        count = 0 
+        candidate = 0
+
+        for num in nums:
+
+            if count == 0:
+                candidate = num
+            
+            if candidate == num:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
         
