351e61d50003860044193ccf231c5deddddb0699
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-08-26 21:38:20 +0530

    Time: 7 ms (32.24%), Space: 19.8 MB (13.1%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,8 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] = 1
+        freq = Counter(nums)
         for num in freq:
             if freq[num] == 1:
                 return num
-
-
-
-
-
         
