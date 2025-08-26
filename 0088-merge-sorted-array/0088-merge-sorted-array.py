5937e6345ea1513e50bbd769c0678d672de91a89
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-08-26 21:12:49 +0530

    Time: 0 ms (100%), Space: 18.2 MB (25.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,8 +1,40 @@
+import math
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        gap = math.ceil((m+n)/2)
+        while gap > 0:
+            i = 0
+            j = gap
+            while j < (m+n):
+                if i < m:
+                    a = nums1[i]
+                else:
+                    a = nums2[i-m]
+                if j < m:
+                    b = nums1[j]
+                else:
+                    b = nums2[j-m]
+                if a > b:
+                    if i < m and j < m:
+                        nums1[i], nums1[j] = nums1[j], nums1[i]
+                    elif i < m and j >= m:
+                        nums1[i] , nums2[j-m] = nums2[j-m], nums1[i]
+                    else:
+                        nums2[i-m], nums2[j-m] = nums2[j-m], nums2[i-m]
+
+                i += 1
+                j += 1
+            
+            if gap == 1:
+                gap = 0
+            else:
+                gap = math.ceil(gap/2)
+        
         nums1[m:] = nums2
-        nums1.sort()
+
+                
+
+
+
+
         
