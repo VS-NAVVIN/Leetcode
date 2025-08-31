fc75000dfc602bb46580880530d27454d839a663
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-08-31 20:02:02 +0530

    Time: 0 ms (100%), Space: 17.7 MB (57.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,27 +1,22 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
-        n = len(nums)
-        i = j = n-1
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
 
-        while i > 0 and nums[i-1] >= nums[i]:
-            i = i - 1
-         
-        if i == 0:
-            nums.reverse()
-            return
+        j = len(nums)-1
+        if i >= 0:
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
 
-        while nums[j] <= nums[i-1]:
-            j = j - 1
-        
-        nums[i-1], nums[j] = nums[j], nums[i-1]
+        low = i+1
+        high = len(nums)-1
 
-        nums[i:] = reversed(nums[i:])
-
-
+        while low <= high:
+            nums[low], nums[high] = nums[high], nums[low]
+            low += 1
+            high -= 1
 
         
 
