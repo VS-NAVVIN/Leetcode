ec6cd94c354ebca040e7c2495fc4340c989e4164
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-09-08 13:21:54 +0530

    Time: 11 ms (25.89%), Space: 18.6 MB (91.78%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,10 @@
+from collections import Counter
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        
         nums.sort()
         n = len(nums)
         for i in range(0,n):
             if i != nums[i]:
                 return i
         return n
-        
