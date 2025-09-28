c535eb5f7e3ab3a8782e711b92961d7aa6b2e293
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-09-28 11:26:57 +0530

    Time: 0 ms (100%), Space: 17.9 MB (30.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
-            return  ""
+            return ""
+        
         prefix = strs[0]
-        for string in strs:
-            while not string.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
-        return prefix 
 
-        
-        
+        for s in strs:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+
+        return prefix
+            
