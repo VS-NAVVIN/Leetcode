8c217a5c2a93757adf0cfba5e11d846f051aa22b
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-02 15:26:36 +0530

    Time: 1 ms (11.33%), Space: 18.4 MB (31.06%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,29 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         
-        low = 0
-        high = len(nums) - 1
-
-        while low <= high:
-
-            mid = (low+high)//2
-            
-            if nums[mid] == target:
-                return True
-            
-            if nums[low] == nums[mid] == nums[high]:
-                low += 1
-                high -= 1
-            elif nums[low] <= nums[mid]:
-                if nums[low] <= target < nums[mid]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
-            else:
-                if nums[mid] < target <= nums[high]:
-                    low = mid+1
-                else:
-                    high = mid - 1
-        
-        return False
-                
+        c = Counter(nums)
+        if target in c.keys():
+            return True
+        else:
+            return False                
