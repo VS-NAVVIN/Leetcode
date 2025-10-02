78777ef61a237b9b6bcc1e5c288bd5548243e9b4
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-02 15:12:13 +0530

    Time: 0 ms (100%), Space: 18.2 MB (22.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,18 +3,17 @@ class Solution:
         low = 0
         high = len(nums) - 1
         while low <= high:
-            mid = (low + high)//2
+            mid = (low+high)// 2
             if nums[mid] == target:
                 return mid
             if nums[low] <= nums[mid]:
-                if nums[low] <= target < nums[mid]:
-                    high = mid-1
+                if nums[low] <= target < nums[mid]: 
+                    high = mid - 1
                 else:
-                    low = mid+1
+                    low = mid+1                
             else:
                 if nums[mid] < target <= nums[high]:
                     low = mid+1
                 else:
-                    high = mid-1
-            
+                    high = mid - 1
         return -1
