90f5d506845a6eae557ce9e28885cee2ba49aad6
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-18 12:54:30 +0530

    Time: 0 ms (100%), Space: 19.9 MB (19.26%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -3,26 +3,13 @@ class Solution:
         
         intervals.append(newInterval)
         intervals.sort()
-        if len(intervals) == 0:
-            return [newInterval]
         merged = [intervals[0]]
 
-        l = intervals[0]
-        if len(intervals) == 1:
-            if l[0] >= newInterval[0]:
-                l[0] = max(l[0], newInterval[0]) 
-            if l[1] >= newInterval[0]:
-                l[1] = max(l[1], newInterval[1])
-            else:
-                merged.append(newInterval)
-            return merged
-
-        for current in intervals[1:]:   
+        for current in intervals[1:]:
             last = merged[-1]
-            if last[1] >= newInterval[0]:
-                last[1] = max(newInterval[1], last[1])
             if last[1] >= current[0]:
                 last[1] = max(last[1], current[1])
             else:
                 merged.append(current)
+                
         return merged
