371ab7d4e48fb9fe6783be3d0fabba2cd3047782
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-20 14:23:17 +0530

    Time: 0 ms (100%), Space: 19.6 MB (31.83%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        n = Counter(nums)
+        for i, count in n.items():
+
+            if count == 1:
+                return i
+        
+        return 0
