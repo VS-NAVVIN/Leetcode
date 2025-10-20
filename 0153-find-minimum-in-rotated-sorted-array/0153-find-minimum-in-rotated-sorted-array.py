1d8618ce5d72cf2959b8176aed00dfec56831747
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-20 18:28:08 +0530

    Time: 0 ms (100%), Space: 17.9 MB (80.14%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,11 +1,4 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # low = 0
-        # high = len(nums) - 1
-
-        # while low <= high:
-
-        #     mid =(low+high)//2
-        
         nums.sort()
         return nums[0]
