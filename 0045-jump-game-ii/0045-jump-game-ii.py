1e286d2d274ac99101c29324b538cf1870dab9e4
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-21 14:01:57 +0530

    Time: 7 ms (79.14%), Space: 18.7 MB (35.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,19 +4,13 @@ class Solution:
         count = 0
         maxjump = 0
         lastjump = 0
-
         if len(nums) < 2:
             return 0
-
         for i in range(len(nums)):
-
             maxjump = max(maxjump, i + nums[i])
-
             if i == lastjump:
-
                 count += 1
                 lastjump = maxjump
-
                 if lastjump >= len(nums)-1:
                     break
         return count
