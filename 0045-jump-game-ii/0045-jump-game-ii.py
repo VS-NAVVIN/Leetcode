1c74f6a6abe61126308ca3bdafd00a687348e99d
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-21 14:01:22 +0530

    Time: 7 ms (79.14%), Space: 18.7 MB (56.2%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        count = 0
+        maxjump = 0
+        lastjump = 0
+
+        if len(nums) < 2:
+            return 0
+
+        for i in range(len(nums)):
+
+            maxjump = max(maxjump, i + nums[i])
+
+            if i == lastjump:
+
+                count += 1
+                lastjump = maxjump
+
+                if lastjump >= len(nums)-1:
+                    break
+        return count
