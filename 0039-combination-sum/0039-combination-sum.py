158cce0787dbab43a8134f093393fa9e05e1e9cf
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-23 19:52:34 +0530

    Time: 3 ms (97.32%), Space: 17.9 MB (39.16%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        result = []
+
+        def backtrack(start, path, total):
+
+            if total > target:
+                return 
+            
+            if total == target:
+                result.append(path[:])
+                return
+            
+            for i in range(start, len(candidates)):
+
+                path.append(candidates[i])
+                backtrack(i, path, total + candidates[i])
+                path.pop()
+            
+        backtrack(0,[],0)
+
+        return result
