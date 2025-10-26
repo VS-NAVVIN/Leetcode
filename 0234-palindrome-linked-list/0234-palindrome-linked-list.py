4569153bec71cb46053e302741cafeb2dfe66eda
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-10-26 12:56:25 +0530

    Time: 17 ms (92.36%), Space: 39.4 MB (27.29%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,35 +6,9 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        def clone(node):
-            dummy = ListNode(-1)
-            tail = dummy
-            while node:
-                tail.next = ListNode(node.val)
-                tail = tail.next
-                node = node.next
-            return dummy.next
-
-        def reverse(node):
-            original = head
-            prev = None
-            curr = head
-
-            while curr:
-            
-                nextnode = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nextnode
-            return prev
-        
-        original = clone(head)
-        copy = reverse(clone(head))
-
-        while original and copy:
-            if original.val != copy.val:
-                return False
-            original = original.next
-            copy = copy.next
-
-        return True
+        arr = []
+        current = head
+        while current:
+            arr.append(current.val)
+            current = current.next
+        return True if arr[::] == arr[::-1] else False
