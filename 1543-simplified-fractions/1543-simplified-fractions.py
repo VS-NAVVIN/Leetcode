a382d2c3150bdecfc7828f8772ba2f8a86321050
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-11-01 18:49:19 +0530

    Time: 18 ms (89.21%), Space: 18.3 MB (88.13%) - LeetHub

---
+++ 1543-simplified-fractions/1543-simplified-fractions.py
@@ -0,0 +1,12 @@
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        
+        res = []
+
+        for i in range(1,n):
+            for j in range(i+1,n+1):
+                if math.gcd(i,j) == 1:
+                    res.append(f"{i}/{j}")
+        return res
+                
+
