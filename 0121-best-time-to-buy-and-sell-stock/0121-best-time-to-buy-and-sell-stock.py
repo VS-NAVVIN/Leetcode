1d8b362d373d41865b5ed8b0beca21b35607eb58
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-11-02 18:43:43 +0530

    Time: 72 ms (69.92%), Space: 27 MB (8.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,8 @@ class Solution:
         
         profit = 0
         buy = prices[0]
-
         for i in prices:
-
             if i < buy:
                 buy = i
             profit = max(profit, i-buy)
-
         return profit
