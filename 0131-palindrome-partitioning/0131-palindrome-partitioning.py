28ce54390344dc0f229e6f82ce23fce6f5c36451
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-11-07 19:04:47 +0530

    Time: 47 ms (68.31%), Space: 34.5 MB (73.54%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,23 +2,16 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
         result = []
-
-        def palindrome(substr):
-            return substr == substr[::-1]
-
         def backtrack(start, path):
-
             if start == len(s):
                 result.append(path[:])
                 return 
-            
             for end in range(start+1, len(s) + 1):
                 substr = s[start:end]
-                if palindrome(substr):
+                if substr == substr[::-1]:
                     path.append(substr)
                     backtrack(end, path)
                     path.pop()
-        
         backtrack(0,[])
         return result
 
