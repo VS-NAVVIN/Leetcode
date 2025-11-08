ea6df57da77e9f34a9349a5bc8c53c6dbc64cb2e
VS-NAVVIN <124044886+VS-NAVVIN@users.noreply.github.com>
2025-11-08 10:09:52 +0530

    Time: 11 ms (52.04%), Space: 18.2 MB (95.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
         maxi = mini = ans = nums[0]
         for i in nums[1:]:
-
             if i < 0:
                 mini, maxi = maxi, mini
             maxi = max(maxi*i, i)
             mini = min(mini*i, i)
             ans = max(ans, maxi)
-
         return ans
 
             
